24f3f664526ff4ee3782a913f230b1bc8367f857
Abdurahman Mukhammedzhan <abdurahmanmuhammedzhan@gmail.com>
2025-01-29 09:38:23 +0500

    Добавьте файлы проекта.

---
+++ Hotel.Atr.RealPortal/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+using Hotel.Atr.RealPortal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.Atr.RealPortal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+
+        public IActionResult Event()
+        {
+            return View();
+        }
+
+    }
+}
